93ca1f4ac99cd548d726b1afe3c7140b561bbee8
Adrian Perea <adrianmarkperea@gmail.com>
2020-04-27 11:32:03 +0900

    Implement Code Splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
-import Navbar from './Navbar';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
+import Loading from './Loading';
+
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const Navbar = React.lazy(() => import('./Navbar'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const Articles = React.lazy(() => import('./Articles'));
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        <React.Suspense fallback={<Loading />}>
+          <Navbar />
 
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/players'>
-            <Players />
-          </Route>
-          <Route path='/teams'>
-            <Teams />
-          </Route>
-          <Route exact path='/:teamId'>
-            <TeamPage />
-          </Route>
-          <Route  path='/:teamId/articles'>
-            <Articles />
-          </Route>
-          <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
-        </Switch>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/players'>
+              <Players />
+            </Route>
+            <Route path='/teams'>
+              <Teams />
+            </Route>
+            <Route exact path='/:teamId'>
+              <TeamPage />
+            </Route>
+            <Route path='/:teamId/articles'>
+              <Articles />
+            </Route>
+            <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
+          </Switch>
+        </React.Suspense>
       </div>
     </Router>
   );
